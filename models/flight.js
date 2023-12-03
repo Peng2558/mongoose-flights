304cc162dd04d9190a7dceb8ca5303ccb984499e
mikega1354 <tabrizimike@gmail.com>
2023-12-02 16:59:50 -0800

    Adds mongoose lab part2

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
+const destinationSchema = new Schema({
+ 
+  airport: {
+
+    type: String,
+    enum: ['AUS','DFW','DEN','LAX','SAN'],
+    default: 'DEN'
+
+   },
+ 
+  arrival: {
+
+   type: Date
+  
+  }
+}, {
+  timestamps: true
+});
 
 const flightSchema = new Schema({
   airline: {
@@ -31,9 +49,8 @@ const flightSchema = new Schema({
    return oneYear
    }
   
-    
-
-  }
+  },
+  destinations :[destinationSchema]
 }, {
   timestamps: true
 });
